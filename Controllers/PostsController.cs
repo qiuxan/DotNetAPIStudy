76692816b0bf65cbdf5a206045bff5d97902db0b
qiuxan <qiuxan@gmail.com>
2024-06-18 11:19:18 +1000

    create CreatPost and Display a post by ID API

--- Controllers/PostsController.cs
+++ Controllers/PostsController.cs
@@ -1,22 +1,33 @@
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using MyFirstApi.Models;
-
-namespace MyFirstApi.Namespace
+using MyFirstApi.Services;
+namespace MyFirstApi.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class PostsController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class PostsController : ControllerBase
+    private readonly PostsService _postsService;
+    public PostsController()
     {
-        [HttpGet]
-        public ActionResult<List<Post>> GetPosts()
+        _postsService = new PostsService();
+    }
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Post>> GetPost(int id)
+    {
+        var post = await _postsService.GetPost(id);
+        if (post == null)
         {
-            return new List<Post>
-            {
-                new() { Id = 1, UserId = 1, Title = "Post1", Body = "The first post." },
-                new() { Id = 2, UserId = 1, Title = "Post2", Body = "The second post." },
-                new() { Id = 3, UserId = 1, Title = "Post3", Body = "The third post." }
-            };
+            return NotFound();
         }
+        return Ok(post);
+    }
+    // Omitted for brevity
+
+    [HttpPost]
+    public async Task<ActionResult<Post>> CreatePost(Post post)
+    {
+        await _postsService.CreatePost(post);
+        return CreatedAtAction(nameof(GetPost), new { id = post.Id },
+        post);
     }
 }
